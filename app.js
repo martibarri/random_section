df998abddc4eee47efc84812d326e88d1ea88f53
marti <martibarri23@gmail.com>
2017-07-25 14:18:16 +0200

    new random

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var app = angular.module("randomApp", ['mb-dragToReorder']);
 
 app.controller("myCtrl", function($scope) {
     $scope.date = new Date();
-    $scope.seccions1 = ["Bar", "Càtering", "CTU", "Escenari", "Engresca't", "Festa de Dia", "Finançament", "Instal·lació", "Màrqueting", "Multimèdia", "Música", "Propaganda", "Seguretat", "VdE", "WIT"];
+    $scope.seccions1 = ["Junta", "Bar", "Càtering", "CTU", "Escenari", "Engresca't", "Festa de Dia", "Finançament", "Instal·lació", "Màrqueting", "Multimèdia", "Música", "Propaganda", "Seguretat", "VdE", "WIT"];
     $scope.seccions2 = [];
     $scope.actual = "";
-    
+    $scope.knife = false;
+    var data_gresca = new Date("2018-04-14T18:00:00.000Z"); 
+    $scope.dies_restants = parseInt((data_gresca - $scope.date) / 1000 / 60 / 60 / 24);
     $scope.next = function() {
-        if($scope.actual){
-            $scope.seccions2.push($scope.actual);    
+        if($scope.seccions1.length!=0){
+            if($scope.actual){
+                $scope.seccions2.push($scope.actual);    
+            }
+            $scope.actual = $scope.seccions1[0];
+            $scope.seccions1.splice(0,1);    
+        } else {
+            $scope.actual = "esca esca esca";
         }
-        $scope.actual = $scope.seccions1[0];
-        $scope.seccions1.splice(0,1);
     };
     $scope.random = function() {
         var m = $scope.seccions1.length, t, i;
@@ -27,5 +33,13 @@ app.controller("myCtrl", function($scope) {
         $scope.seccions2 = [];
         $scope.actual = "";
     };
-    
+    $scope.keydown = function(event) {
+        console.log(event);
+        if (event.charCode == 107) {
+            $scope.knife = true;
+        }
+        if (event.charCode == 106) {
+            $scope.knife = false;
+        }
+    };
 }); 
