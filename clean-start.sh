b11b778fa70897a5fcec98c54800634249a8a100
marti <martibarri23@gmail.com>
2017-09-15 00:19:49 +0200

    Dockerize

--- clean-start.sh
+++ clean-start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-docker build -t random_section .
+echo
+echo '***********************'
+echo '*** Stopping Docker ***'
+echo '***********************'
+echo
+docker stop random_section
+echo
+echo '**********************'
+echo '*** Removing Image ***'
+echo '**********************'
+echo
+docker rm random_section
+echo
+echo '************************************************'
+echo '*** Deploy random_section Docker on port 8050***'
+echo '************************************************'
+echo
+docker run -d --name random_section -p 127.0.0.1:8050:80 -v $PWD/reunio_app:/usr/share/nginx/html:ro nginx
 
-docker run -d -p 80:80 random_section
